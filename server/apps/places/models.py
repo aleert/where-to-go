c93e7cf2d60bd0b63fb81517fd0cd2bf8feefbdb
Aleksei Panfilov <aleert@yandex.ru>
2020-06-14 22:24:12 +0300

    Add Place model.

--- server/apps/places/models.py
+++ server/apps/places/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from model_utils.models import TimeStampedModel
+
+
+class Place(TimeStampedModel):
+
+    title = models.CharField(_('title'), max_length=255)
+    description_short = models.CharField(_('short description'), max_length=255, blank=True)
+    description_long = models.TextField(_('description'))
+    lat = models.FloatField(_('latitude'))
+    lon = models.FloatField(_('longitude'))
+
+    def __str__(self):
+        return self.title
