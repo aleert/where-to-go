29d73d0ef42f584ce953aff2f76d2c57f7a05ed2
Aleksei Panfilov <aleert@yandex.ru>
2020-06-14 22:03:03 +0300

    Create places app.

--- server/settings/base.py
+++ server/settings/base.py
@@ -81,6 +81,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     'server.apps.users.apps.UsersConfig',
+    'server.apps.places.apps.PlacesConfig',
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
