8f88b71503541bbee79ca3552e2a8d817489484d
Aleksei Panfilov <aleert@yandex.ru>
2020-06-16 00:08:52 +0300

    Add Image model, models linting.

--- server/apps/places/models.py
+++ server/apps/places/models.py
@@ -1,16 +1,35 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
 from model_utils.models import TimeStampedModel
 
 
 class Place(TimeStampedModel):
 
-    title = models.CharField(_('title'), max_length=255)
-    description_short = models.CharField(_('short description'), max_length=255, blank=True)
+    title = models.CharField(_('title'), max_length=255)  # noqa: WPS432
+    description_short = models.CharField(
+        _('short description'), max_length=255, blank=True,  # noqa: WPS432
+    )
     description_long = models.TextField(_('description'))
     lat = models.FloatField(_('latitude'))
     lon = models.FloatField(_('longitude'))
 
+    class Meta:
+        verbose_name = _('Place')
+        verbose_name_plural = _('Places')
+
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+
+    location = models.ImageField(_('Image location'), upload_to='places/%Y/%m')
+    place = models.ForeignKey(to='Place', on_delete=models.CASCADE)
+
+    class Meta:
+        order_with_respect_to = 'place'
+        verbose_name = _('Image')
+        verbose_name_plural = _('Images')
+
+    def __str__(self):
+        return f'Picture {self._order} of {self.place}'
