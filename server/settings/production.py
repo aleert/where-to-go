a7d73b960db23038bc9c40ae27306e4ab118bb9f
Aleksei Panfilov <aleert@yandex.ru>
2020-06-18 22:29:31 +0300

    Fix typo.

--- server/settings/production.py
+++ server/settings/production.py
@@ -18,7 +18,7 @@ DATABASES = {
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': config('POSTGRES_HOST'),
-        'PORT': config('POSTGRES_HOST', cast=int),
+        'PORT': config('POSTGRES_PORT', cast=int),
         'CONN_MAX_AGE': config('CONN_MAX_AGE', cast=int, default=60),
         'OPTIONS': {
             'connect_timeout': 10,
