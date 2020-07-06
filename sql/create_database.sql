a2984a94ec8f7283dcc96fc2fb7d338f117d760c
Aleksei Panfilov <aleert@yandex.ru>
2020-07-06 19:28:44 +0300

    Add sql to manage database creation.

---
+++ sql/create_database.sql
@@ -0,0 +1,3 @@
+CREATE USER eqyuyotdamao  WITH PASSWORD 'JW86aTPOfgAu';
+CREATE DATABASE test_pure_postgres OWNER eqyuyotdamao ENCODING 'utf-8';
+CREATE DATABASE test_postgis OWNER eqyuyotdamao ENCODING 'utf-8';
---
+++ sql/drop_database.sql
@@ -0,0 +1,3 @@
+DROP DATABASE test_pure_postgres;
+DROP DATABASE test_postgis;
+DROP USER eqyuyotdamao;
