3f2bcbc95e6edb930a9844ede6345fc2d190704d
Aleksei Panfilov <aleert@yandex.ru>
2020-07-06 19:49:22 +0300

    Add pure_postgres models and migrations.

--- server/apps/places/models.py
+++ server/apps/places/models.py
@@ -1,7 +1,9 @@
 from django.contrib.postgres.indexes import GistIndex
-from django.contrib.gis.db import models
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 from tinymce.models import HTMLField
+
+from server.apps.places.fields import PointField
 
 
 class Place(models.Model):
@@ -11,11 +13,14 @@ class Place(models.Model):
         _('short description'), max_length=1000, blank=True,
     )
     description_long = HTMLField(_('description'))
-    coord = models.PointField(_('coordinates (lng, lat)'))
+    coord = PointField(_('coordinates (lng, lat)'))
 
     class Meta:
         verbose_name = _('Place')
         verbose_name_plural = _('Places')
+        indexes = (
+            GistIndex(fields=['coord']),
+        )
 
     def __str__(self):
         return self.title
@@ -25,8 +30,13 @@ class Image(models.Model):
 
     location = models.ImageField(_('Image location'), upload_to='places/%Y/%m')
     place = models.ForeignKey(to='Place', on_delete=models.CASCADE)
+    _order = models.PositiveIntegerField(default=0, blank=False, null=False)
 
     class Meta:
+        ordering = ('_order', )
         verbose_name = _('Image')
         verbose_name_plural = _('Images')
 
+    def __str__(self):
+        pattern = _('Picture {order} of Place {title}')
+        return pattern.format(order=self._order, title=self.place.title)
